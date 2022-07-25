de97dbccaed9c9d8d4be08ccd87181b02432ba11
rashedrafi <rashed.bin.nur@gmail.com>
2022-07-24 22:52:46 -0400

    new class

---
+++ src/main/java/Jewelry/Diamondcity/newclass1.java
@@ -0,0 +1,20 @@
+package Jewelry.Diamondcity;
+
+public class newclass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+
+	}
+
+	
+	public static void lession () {
+		
+	}	
+	
+	
+	
+}
