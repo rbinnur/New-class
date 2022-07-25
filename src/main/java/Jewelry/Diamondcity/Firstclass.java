a5f2ce672290be73e02fc97a917702a5c187fa16
rashedrafi <rashed.bin.nur@gmail.com>
2022-07-24 22:02:57 -0400

    first commit

---
+++ src/main/java/Jewelry/Diamondcity/Firstclass.java
@@ -0,0 +1,26 @@
+package Jewelry.Diamondcity;
+
+public class Firstclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+			office();
+			
+
+		
+
+	}
+	
+	
+	public static void office () {
+		
+		System.out.println("This is my first class");
+		
+	}
+	
+	
+	
+
+}
